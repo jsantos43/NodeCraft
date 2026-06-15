d962e8664b274b606f6ff09cc6e7d29e80d77ab0
João Pedro Tomaz dos Santos <joao.santos.2007sp@gmail.com>
2026-06-14 21:41:45 -0300

    update

--- manager/db/migrations/20260614000001-fix-worker-lastSeenAt-type.js
+++ manager/db/migrations/20260614000001-fix-worker-lastSeenAt-type.js
@@ -3,14 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('workers', 'lastSeenAt', {
+    await queryInterface.changeColumn('worker', 'lastSeenAt', {
       type: Sequelize.DataTypes.BIGINT,
       allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('workers', 'lastSeenAt', {
+    await queryInterface.changeColumn('worker', 'lastSeenAt', {
       type: Sequelize.DataTypes.TIME,
       allowNull: true,
     });
